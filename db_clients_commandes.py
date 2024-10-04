eb690ce06c026f941e438064055d15208a23ab93
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-04 16:05:26 +0200

    Add detailed docstrings to all functions
    Each function now includes detailed docstrings describing the parameters and return values. These updates improve code readability and help maintain consistency across the module.

--- db_clients_commandes.py
+++ db_clients_commandes.py
@@ -43,20 +43,32 @@ SQL_INSERT_FROM_CSV = {
 }
 
 def get_db_connection(db_name):
+    """
+    :param db_name: Name of the database to connect to
+    :return: SQLite database connection object
+    """
     connection = sqlite3.connect(db_name)
     return connection
 
 def create_tables(db_connection):
+    """
+    :param db_connection: Database connection object used to connect to the database.
+    :return: None
+    """
     cursor = db_connection.cursor()
     for sql_create in SQL_CREATE_TABLES.values():
         cursor.execute(sql_create)
     db_connection.commit()
 
 def insert_clients(table_csv, db_connection):
-
-        cursor = db_connection.cursor()
-        for row in table_csv:
-            client = {
+    """
+    :param table_csv: A list of lists, where each interior list represents a row of client data to be inserted into the database.
+    :param db_connection: A database connection object that manages the database connection.
+    :return: None
+    """
+    cursor = db_connection.cursor()
+    for row in table_csv:
+        client = {
                 "ID": row[0],
                 "nom": row[1],
                 "prenom": row[2],
@@ -65,26 +77,35 @@ def insert_clients(table_csv, db_connection):
                 "ddn": row[5],
                 "adresse": row[6],
                 "marketing_consent": row[7]
-            }
-            cursor.execute(SQL_INSERT_FROM_CSV["client"], client)
-
-        db_connection.commit()
+        }
+        cursor.execute(SQL_INSERT_FROM_CSV["client"], client)
+    db_connection.commit()
 
 
 def insert_commandes(table_csv, db_connection):
-        cursor = db_connection.cursor()
-        for row in table_csv:
-            commande = {
-                "client_ID": row[1],
-                "date": row[2],
-                "montant": row[3]
-            }
-            cursor.execute(SQL_INSERT_FROM_CSV["commande"], commande)
+    """
+    :param table_csv: List of rows where each row is a list containing order details.
+    :param db_connection: Active database connection object.
+    :return: None
+    """
+    cursor = db_connection.cursor()
+    for row in table_csv:
+        commande = {
+            "client_ID": row[1],
+            "date": row[2],
+            "montant": row[3]
+        }
+        cursor.execute(SQL_INSERT_FROM_CSV["commande"], commande)
 
-        db_connection.commit()
+    db_connection.commit()
 
 
 def insert_from_csv(entity_name, db_connection):
+    """
+    :param entity_name: The name of the entity to be inserted into the database. This should match a table name in the database.
+    :param db_connection: The database connection object used to insert data.
+    :return: None
+    """
     # Open and read the CSV file
     csv_file = entity_name + ".csv"
     with (open(csv_file, newline='', encoding='utf-8') as csvfile):
@@ -104,6 +125,10 @@ def insert_from_csv(entity_name, db_connection):
             insert_commandes(table_csv, db_connection)
 
 def print_clients_with_consent(db_connection):
+    """
+    :param db_connection: The database connection to be used for querying the Clients table.
+    :return: None
+    """
     cursor = db_connection.cursor()
     query = """
     SELECT 
@@ -121,6 +146,11 @@ def print_clients_with_consent(db_connection):
     print("Nb clients: ", len(clients))
 
 def print_client_commandes(client_ID, db_connection):
+    """
+    :param client_ID: The unique identifier of the client whose orders are to be printed.
+    :param db_connection: A database connection object used to execute queries.
+    :return: None
+    """
     cursor = db_connection.cursor()
     print("COMMANDES pour le client avec l'ID " + str(client_ID))
     query_commandes = "SELECT * FROM Commandes WHERE client_ID = " + str(client_ID)
@@ -131,6 +161,10 @@ def print_client_commandes(client_ID, db_connection):
     print("Nb commandes: ", len(commandes))
 
 def get_random_client_ID(db_connection):
+    """
+    :param db_connection: Database connection object to execute the query.
+    :return: Randomly selected client ID from the Clients table.
+    """
     cursor = db_connection.cursor()
     query = """
         SELECT 
@@ -146,6 +180,11 @@ def get_random_client_ID(db_connection):
     return client_id
 
 def print_montant_commandes(client_id, db_connection):
+    """
+    :param client_id: The ID of the client whose total order amount is to be calculated.
+    :param db_connection: The database connection object used to execute the query.
+    :return: None
+    """
     cursor = db_connection.cursor()
     query = "SELECT SUM(montant) FROM Commandes WHERE client_ID = " + str(client_id)
     cursor.execute(query)
@@ -153,6 +192,11 @@ def print_montant_commandes(client_id, db_connection):
     print("Montant total commandes : " + str(montant_total))
 
 def print_clients_montant_commandes_over(montant, db_connection):
+    """
+    :param montant: The minimum order amount to filter clients with orders over this amount.
+    :param db_connection: The database connection object to execute the query.
+    :return: None
+    """
     cursor = db_connection.cursor()
     query = "SELECT * FROM Clients as cl INNER JOIN Commandes as cm ON cl.ID = cm.client_ID WHERE cm.montant > " + str(montant)
     cursor.execute(query)
@@ -163,6 +207,11 @@ def print_clients_montant_commandes_over(montant, db_connection):
     print("Nb clients: ", len(clients))
 
 def print_clients_date_commandes_over(date, db_connection):
+    """
+    :param date: Date object to filter commandes that are after this date.
+    :param db_connection: Database connection object used to execute queries.
+    :return: None
+    """
     cursor = db_connection.cursor()
     query = "SELECT * FROM Clients as cl INNER JOIN Commandes as cm ON cl.ID = cm.client_ID WHERE cm.date > " + str(date)
     cursor.execute(query)
@@ -173,6 +222,10 @@ def print_clients_date_commandes_over(date, db_connection):
     print("Nb clients: ", len(clients))
 
 def deleteTables(db_connection):
+    """
+    :param db_connection: The database connection object.
+    :return: None. This function drops the 'Clients' and 'Commandes' tables from the database and commits the changes.
+    """
     cursor = db_connection.cursor()
     cursor.execute("DROP TABLE IF EXISTS Clients")
     cursor.execute("DROP TABLE IF EXISTS Commandes")
